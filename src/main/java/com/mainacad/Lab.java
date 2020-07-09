4dbe5355bb4cd12fc51408530721964b371b4793
Name <Name@gma.com>
2020-07-09 21:51:17 +0300

    1

--- src/main/java/com/mainacad/Lab.java
+++ src/main/java/com/mainacad/Lab.java
@@ -1,31 +1,37 @@
 package com.mainacad;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 
 public class Lab
 {
+    private static final By HEADER = By.xpath("//div[@class='main-header']");
+
     public static void main( String[] args )
     {
+
         //Создаём системную переменную которая содержит путь к драйверу
-        System.setProperty("webdriver.chrome.driver","/drivers/chromedriver.exe");
-
+        //   System.setProperty("webdriver.chrome.driver","D:\\Java\\Tools\\chromedriver.exe");
+        WebDriverManager.chromedriver().setup();
         //Создаём вебдрайвер
-        //WebDriver driver = new ChromeDriver();
-        WebDriver driver = new FirefoxDriver();
-
-        //Открываем тестовую страницу
-        driver.get("https://www.toolsqa.com/automation-practice-form/");
+        WebDriver driver = new ChromeDriver();
+                //Открываем тестовую страницу
+        driver.get("https://demoqa.com/automation-practice-form");
 
         //TODO: Вывести в консоль текст Инфо сообщения
 
-        System.out.println();
+       // System.out.println();
 
         //TODO: Вывести в консоль текст заголовка формы ("Practice Automation Form")
+        System.out.println("Текст заголовка" + " " + driver.findElement(HEADER).getText());
 
         //TODO: Кликнуть линк Partial Link Test
+        driver.findElement(By.partialLinkText("Forms")).click();
 
         //TODO: Заполнить поле First name:
 
